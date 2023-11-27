7e42e7ccc8ec3b0cdb00b6639f4336f5a7cb4f47
Your Name <tajempey@gmail.com>
2023-11-27 16:09:25 -0700

    getChange() test 1 red
    Co-authored by: Hunter Empey
    <LizardMak@gmail.com>

---
+++ __tests__/keepCount.test.js
@@ -0,0 +1,9 @@
+import getChange from "../src/keepCount";
+
+describe('getChange', () => {
+
+  test('Should subtract 0.25 from 4.99 until the value is less than 0.25', () => {
+    const valueTest = getChange(4.99);
+    expect(valueTest).toEqual(0.24);
+  });
+});
